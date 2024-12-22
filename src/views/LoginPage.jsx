0de28044fa6df144f809d32ca7bc6b73ea210f02
Upasana Sharma <sharmaupasana823@gmail.com>
2024-12-22 19:32:20 +0100

    feat: Login and register and Homepage frontend

---
+++ src/views/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Auth from '../components/auth';
+
+function LoginPage() {
+  return (
+    <div className="w-full ">
+      <Auth type="login" />
+    </div>
+  );
+}
+
+export default LoginPage;
